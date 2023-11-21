d229326c812501115a99d61afd765cb6e330dc3a
brenden <brendenjarias@gmail.com>
2023-11-20 21:10:13 -0800

    completed exercise

--- part_2/part_2.js
+++ part_2/part_2.js
@@ -1,6 +1,8 @@
 //part 2
 
-const baseURL = 'https://deckofcardsapi.com/api/deck/'
+const baseURL = 'https://deckofcardsapi.com/api/deck/';
+const pullCardBtn = document.getElementById('pullCardBtn');
+const cardPile = document.getElementById('cardPile');
 
 //part 2.1
 async function getOneCard() {
@@ -36,3 +38,27 @@ async function getTwoCards() {
 }
 
 //part 3.3
+let deckId;
+async function drawCard() {
+    let res;
+    try {
+        if(!deckId) {
+            res = await axios.get(`${baseURL}/new/draw`)
+            deckId = res.data.deck_id;
+        } else {
+            res = await axios.get(`${baseURL}/${deckId}/draw`)
+        }
+        if(res.data.remaining === 0) {
+            pullCardBtn.disabled = true;
+        }
+        const cardImgEle = document.createElement('img');
+        cardImgEle.classList.add('card');
+        cardImgEle.src = res.data.cards[0].images.svg;
+        cardImgEle.style.transform = `translate(${Math.random() * (60 - 40) + 40}px, ${Math.random() * (60 - 40) + 40}px) rotate(${-40 + Math.random() * 80}deg)`;
+        cardPile.appendChild(cardImgEle);
+    }
+    catch (error) {
+        console.log(error);
+    };
+};
+pullCardBtn.addEventListener('click', drawCard);
